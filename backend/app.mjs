ac099f9c81462ec86a79cfaa222a09c027bf6bec
abejith <abejithp@hotmail.com>
2024-01-26 09:54:56 -0500

    updated progression with filter

--- backend/app.mjs
+++ backend/app.mjs
@@ -29,7 +29,7 @@ app.use(session({
     cookie: {
         maxAge: SESSION_TIME * 1000,
         secure: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         httpOnly: true
     },
     saveUninitialized: true,
