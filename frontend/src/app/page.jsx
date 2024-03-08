92d529341553003b98fb64789011da5289054dfa
abejith <abejithp@hotmail.com>
2024-03-08 12:59:23 -0500

    redesigned landing page -added tailwindcss -added shadcn

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,13 +1,12 @@
 'use client'
 import './main.css'
 
-import Link from 'next/link';
 import { Login, Register } from '../components/Auth/Auth'
 import { useRef, useState } from "react";
 
+
+
 export default function Home() {
-	const [user, setUser] = useState("")
-	const [password, setPassword] = useState("")
 
 	const loginRef = useRef(null)
 	const aboutRef = useRef(null)
@@ -21,113 +20,56 @@ export default function Home() {
 		});
 	}
 
-	return (<>
-		<div className="main">
-			<div className="intro">
-
-				<div className="taskbar">
-					<div className="container">
-						<div className="scroll" onClick={() => { scroll(aboutRef.current) }} >About</div>
-						<div className="scroll" onClick={() => { scroll(featureRef.current) }} >Features</div>
-						<div className='scroll' onClick={() => { scroll(loginRef.current) }} >Join</div>
-					</div>
+	return (
+		<div className=" flex-col bg-neutral-900 h-auto font-satoshi overflow-hidden">
+			<div className="flex w-full py-[2rem] fixed top-0 justify-center z-[100]">
+				<div className="flex p-[1rem] px-[3rem] gap-[2rem] rounded-full text-white bg-indigo-800 w-fit
+								font-satoshi font-medium cursor-pointer">
+					<div onClick={() => { scroll(aboutRef.current) }} >About</div>
+					<div onClick={() => { scroll(featureRef.current) }} >Features</div>
+					<div onClick={() => { scroll(loginRef.current) }} >Join</div>
 				</div>
+			</div>
 
-				<div className="title">
+
+			<div className="flex h-screen justify-center items-center relative">
+				<div className="flex h-[60%] aspect-square bg-blue-700 absolute left-[5%] top-[15%] rounded-full
+									blur-[100px] opacity-40"></div>
+				<div className="flex h-[70%] aspect-square bg-purple-900 absolute right-[5%] bottom-[5%] rounded-full
+									blur-[100px] opacity-30"></div>
+				<div className=" text-white text-[13rem] font-satoshi font-normal z-10">
 					Inspire
 				</div>
+			</div>
 
-				<div className="about" ref={aboutRef}>
-
-					<div className="content">
-						Inspire is designed to help users track each workout and progress more efficiently
-					</div>
+			<div className="flex h-screen w-auto text-white justify-evenly items-center font-light relative pt-[4rem]" ref={aboutRef}>
+				<div className="flex w-[60%] text-[3rem] text-left z-30 absolute left-[10%]">
+					Empowering individuals to achieve their fitness goals by providing a personalized and accessible
+					platform.
 				</div>
-
-				<div className="feature" ref={featureRef}>
-
-					<div className="card">
-						<div className="left">
-							<div className="display">
-								<div className="column">
-									<div className="day">S</div>
-									<div className="workout">
-										<div className="exercise"></div>
-									</div>
-								</div>
-								<div className="column">
-									<div className="day">M</div>
-									<div className="workout">
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-									</div>
-								</div>
-								<div className="column">
-									<div className="day">T</div>
-									<div className="workout">
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-									</div>
-								</div>
-								<div className="column">
-									<div className="day">W</div>
-									<div className="workout"></div>
-								</div>
-								<div className="column">
-									<div className="day">T</div>
-									<div className="workout">
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-									</div>
-								</div>
-								<div className="column">
-									<div className="day">F</div>
-									<div className="workout">
-										<div className="exercise"></div>
-										<div className="exercise"></div>
-									</div>
-								</div>
-								<div className="column">
-									<div className="day">S</div>
-									<div className="workout"></div>
-								</div>
-							</div>
-						</div>
-						<div className="right">
-							Create a Workout Schedule that aligns with your goals
-						</div>
-					</div>
-
-				</div>
-
-				<div className="join" ref={loginRef}>
-					<div className="left">
-						Join The Community
-					</div>
-					<div className="right">
-						<div className="form">
-							<div className="input-container">
-								<input type="text" placeholder='Username' value={user} onChange={e => setUser(e.target.value)} required={true} />
-								<input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} required={true} />
-							</div>
-
-							<div className="btn-container">
-								<Login user={user} password={password} className="login-btn" />
-								<Register user={user} password={password} className="login-btn" />
-							</div>
-
-						</div>
-					</div>
+				<div className="flex w-[45%] aspect-square justify-center items-center bg-indigo-800 rounded-full absolute right-[-25%] ">
+					<img src="./assets/bottle.png" className='h-[120%] scale-x-[-1] rotate-[-15deg] absolute right-[15%] top-[-20%] ' />
 				</div>
 
 			</div>
 
-		</div>
+			<div className="flex h-screen text-white justify-center items-center pt-[6rem]" ref={featureRef}>
+				<div className="flex h-[70%] aspect-square rounded-full bg-sky-700 blur-[100px] opacity-40">
 
-	</>)
+				</div>
+				<div className="flex text-white z-20 absolute font-light text-[3rem]">
+					Track your progress and Improve daily
+				</div>
+			</div>
+
+			<div className="flex h-screen justify-center items-center pt-[6rem]" ref={loginRef}>
+				<div className="flex bg-indigo-800 p-16 rounded-2xl shadow-2xl">
+					<Login />
+				</div>
+			
+			</div>
+
+
+
+		</div>)
 }
--- frontend/src/components/Auth/Auth.jsx
+++ frontend/src/components/Auth/Auth.jsx
@@ -3,25 +3,68 @@
 import './auth.css'
 import { login, register, logout } from "../../api/auth.mjs"
 
-function handleSubmit(res,err) {
+import { Button } from "@/components/ui/button"
+
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from 'react-hook-form';
+
+function handleSubmit(res, err) {
     if (!res) {
-       return console.log("FAILED")
+        return console.log("FAILED")
     }
 
     window.location.href = '/dashboard'
 }
 
-export function Login(props) {
-    const { user, password } = props
+function handleLogin(data) {
+    login(data.username, data.password).then((res) => handleSubmit(res))
+}
+
+export function Login() {
+    const form = useForm();
 
     return (<>
-          <button onClick={function () {
-            if (user != '' && password != '') {
-                login(user, password).then((res, err) => handleSubmit(res,err))
-            }
-        }} className="auth-btn">
-            Login
-        </button>
+        <Form {...form}>
+            <form className="space-y-8" onSubmit={form.handleSubmit(handleLogin)}>
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem className="flex-col gap-8">
+                            <FormLabel className="text-white font-satoshi font-medium">Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="username" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem className="flex-col gap-8">
+                            <FormLabel className="text-white font-satoshi font-medium">Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="password" {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
     </>)
 }
 
