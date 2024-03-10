aab55bc3e8b4a0c3da6fdcc2db040b2369328d8f
Abejithp <abejithp@hotmail.com>
2024-03-10 15:10:06 -0400

    created responsive sidebar

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,25 +1,12 @@
-'use client'
+import Sidebar from "@/components/Sidebar/Sidebar";
 
-import Taskbar from "@/components/Taskbar/Taskbar"
-import './dashboard.css'
-import { useState } from "react"
-
-import Dashboard from "@/pages/dashboard/Dashboard"
-import Workout from "@/pages/workout/Workout"
-import Progression from "@/pages/progression/Progression"
-
-
-export default function dashboard() {
-    const [tab, updateTab] = useState("Dashboard");
-    const [active, updateWorkout] = useState("");
-
-    return (<>
-        <div className="main">
-            <Taskbar updateTab={updateTab} />
-
-            <div className="content">
-                {(tab=="Dashboard" ? <Dashboard /> : tab =="Workout"? <Workout /> : <Progression />)}
-            </div>
+function Dashboard() {
+    return (
+        <div className="flex">
+            <Sidebar />
         </div>
-    </>)
+    
+    );
 }
+
+export default Dashboard;
