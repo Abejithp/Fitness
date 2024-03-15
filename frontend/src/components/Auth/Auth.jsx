57f6ebf12a297c9b1fd548d1b7e48eeedda61885
abejith <abejithp@hotmail.com>
2024-03-15 12:50:53 -0400

    updated auth ui

--- frontend/src/components/Auth/Auth.jsx
+++ frontend/src/components/Auth/Auth.jsx
@@ -6,13 +6,13 @@ import { login, register, logout } from "../../api/auth.mjs"
 import { Button } from "@/components/ui/button"
 
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from 'react-hook-form';
@@ -29,24 +29,25 @@ function handleLogin(data) {
     login(data.username, data.password).then((res) => handleSubmit(res))
 }
 
+function handleRegister(data) {
+    register(data.username, data.password).then((res) => handleSubmit(res))
+}
+
 export function Login() {
     const form = useForm();
 
     return (<>
         <Form {...form}>
-            <form className="space-y-8" onSubmit={form.handleSubmit(handleLogin)}>
+            <form className="space-y-4 w-full" onSubmit={form.handleSubmit(handleLogin)}>
                 <FormField
                     control={form.control}
                     name="username"
                     render={({ field }) => (
-                        <FormItem className="flex-col gap-8">
-                            <FormLabel className="text-white font-satoshi font-medium">Username</FormLabel>
+                        <FormItem>
+
                             <FormControl>
                                 <Input placeholder="username" {...field} />
                             </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
                         </FormItem>
                     )}
                 />
@@ -54,30 +55,53 @@ export function Login() {
                     control={form.control}
                     name="password"
                     render={({ field }) => (
-                        <FormItem className="flex-col gap-8">
-                            <FormLabel className="text-white font-satoshi font-medium">Password</FormLabel>
+                        <FormItem>
+
                             <FormControl>
                                 <Input type="password" placeholder="password" {...field} />
                             </FormControl>
                         </FormItem>
                     )}
                 />
-                <Button type="submit" className=" bg-slate-600">Submit</Button>
+                <Button type="submit" className=" bg-slate-600 w-full">Login</Button>
             </form>
         </Form>
     </>)
 }
 
-export function Register(props) {
-    const { user, password } = props
+export function Register() {
+    const form = useForm();
+
     return (<>
-        <button onClick={function () {
-            if (user != '' && password != '') {
-                register(user, password).then((res) => handleSubmit(res))
-            }
-        }} className='auth-btn'>
-            Register
-        </button>
+        <Form {...form}>
+            <form className="space-y-4 w-full" onSubmit={form.handleSubmit(handleRegister)}>
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+
+                            <FormControl>
+                                <Input placeholder="username" {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+
+                            <FormControl>
+                                <Input type="password" placeholder="password" {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className=" bg-slate-600 w-full">Sign Up</Button>
+            </form>
+        </Form>
     </>)
 }
 
